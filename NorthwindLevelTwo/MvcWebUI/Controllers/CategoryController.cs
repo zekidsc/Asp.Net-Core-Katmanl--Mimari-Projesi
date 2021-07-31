d6d9225156a17d1c3deffc4913ff02679b6b96f0
Zeki Dişci <zkdsc1996@gmail.com>
2021-07-31 03:12:44 +0300

    Asp.Net Core MVC Katmanlı Mimari
    Asp.Net Core MVC yapısı ile katmanlı mimaride veritabanı ile geliştirmeler.

---
+++ NorthwindLevelTwo/MvcWebUI/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using MvcWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcWebUI.Controllers
+{
+    public class CategoryController:Controller
+    {
+        private ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    var model = new CategoryListViewModel
+        //    {
+        //        Categories = _categoryService.GetAll()
+        //    };
+
+        //    return View(model);
+        //}
+    }
+}
